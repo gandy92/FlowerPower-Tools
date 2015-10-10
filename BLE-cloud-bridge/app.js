0221508d97c68edb332efdb88f7f9379536c2b80
Andy <andy.thaller@gmx.de>
2015-10-10 21:18:07 +0200

    Complete overhaul of sensor handling
    - Use async.waterfall instead of async.serial for proper error handling
    - Tidy up code, add more comments to improve readability
    - Move readout and upload code to seperate function for readability
    - Use structured sensors array to keep sensorSerial/uuid relation
    - Avoid global variables where possible
    - Catch any exceptions thrown by node-flower-power code
    - Readout sensors in same order they where discovered

--- BLE-cloud-bridge/app.js
+++ BLE-cloud-bridge/app.js
@@ -7,14 +7,6 @@ var lazy = require("lazy");
 var asyncFunction = require("sync");
 var uuid = require('node-uuid');
 
-var sessionPeriod = "";
-var sUpTime = "";
-var sessionStartIndex = "";
-var currentID = "";
-var historic = "";
-var uuid = "";
-var user_config_version1 = "";
-
 var clientID     = '';
 var clientSecret = '';
 var userName     = '';
@@ -22,29 +14,20 @@ var passPhrase   = '';
 
 var api = '';
 
-var loop1 = 0;
-var loop2 = 0;
-var loop3 = 0;
+var debug= 0;
+var dry_run= 0; // set dry_run to 1 to avoid uploading data to cloud
 
-var order = 0;
-
-var startIdx = 0;
-var NbrEntries = 0;
-var lastEntry = 0;
-
-var firstPartUuid = 6;
-var lastPartUuid = 10;
 var defaultIndexRecovery = 200;
 var maxIndexRecovery = 3840;
 var waitTimeoutMs = 10000;
 
-var tab = [];
-var tabSensors = [];
-var tabIndex = [];
 var uuidTab = [];
 var param = [];
+var devices = [];
 
-async.series([
+// instead of async.series(), use async.waterfall() and callback(err) to skip to final callback(err,result)
+//
+async.waterfall([
 
   function(callback) {
     var p = 0;
@@ -76,6 +59,9 @@ function(callback) {
   callback();
 },
 
+// ---------------------------------------------------------------------------------
+// login to cloud api and retrieve list of sensors
+//
 function(callback) {
   api.login(userName, passPhrase, function(err) {
     if (!!err) return console.log('login error: ' + err.message);
@@ -83,147 +69,190 @@ function(callback) {
     api.getGarden(function(err, sensors, tabSensorSerial, tabHistoryIndex, user_config_version) {
       if (!!err) return console.log('getGarden: ' + err.message);
 
+      if (4<=debug) console.log("sensors= ",sensors);
+
       user_config_version1 = user_config_version;
-      tabSensors = tabSensorSerial;
-      tabIndex = tabHistoryIndex;
-      console.log('user_config_version1=' + user_config_version1 + ' tabSensors=' + tabSensors + ' tabIndex=' + tabIndex);
+
+      if (2<=debug) console.log('user_config_version1=' + user_config_version1 + ' tabSensors=' + tabSensorSerial + ' tabIndex=' + tabHistoryIndex);
+
+      for (i= 0; i< tabSensorSerial.length; i++)
+      {
+        var uuid= tabSensorSerial[i].toLowerCase();
+        // from serial (e.g. 9003B7 0000 C7476B), use first 6 and last six characters for uuid:
+        uuid= uuid.substr(0,6)+uuid.substr(10,6);
+
+        devices[uuid] = { uuid: uuid, api: {sensorSerial: tabSensorSerial[i], historyIndex: tabHistoryIndex[i], name: sensors[tabSensorSerial[i]].nickname}, state: 'unconfirmed' };
+        if (3<=debug) console.log("cloud device: ",devices[uuid]);
+      }
       callback();
     });
   }).on('error', function(err) {
     console.log('background error: ' + err.message);
+    callback("error during cloud api access: "+err.message);
   });
 },
 
-function(callback) {
-  function discoUuid(){
-    if (loop1 === tabSensors.length) {
-      callback();
-    }
-
-    else {
-      loop3 = 0;
-      var uuid1 = tabSensors[loop1].toLowerCase();
-      var res = uuid1.split("");
-
-      for(t = 0; t < firstPartUuid; t++){
-        uuid += res[t];
-      }
-      for(t = lastPartUuid; t < res.length; t++){
-        uuid += res[t];
-      }
-      // console.log("i=" + uuidTab.length + " uuid=" + uuid);
-      uuidTab.push(uuid);
-      loop1 ++;
-      uuid = "";
-      discoUuid();
-    }
-  };
-  discoUuid();
-},
-
+// ---------------------------------------------------------------------------------
+// scan for available Flower Power sensors and update device list
+//
 function(callback) {
   var timeout;
   loop1 = 0;
+  console.log("Start of Discovery..");
   FlowerPower.discoverAll(function onDiscover(flowerPower) {
     clearTimeout(timeout);
-    console.log(flowerPower.uuid)
+    var uuid= flowerPower.uuid;
+    if (devices[flowerPower.uuid]) {
+      console.log("found registered sensor \""+uuid+"\", \""+devices[uuid].api.name+"\"");
+      devices[uuid].sensor= flowerPower;
+      devices[uuid].state= 'confirmed';
+      uuidTab.push(uuid);
+    }
+    else {
+      console.log("found unknown sensor \""+flowerPower.uuid+"\" - please consider registering with cloud api");
+    }
+    
     asyncFunction(timeout = setTimeout(function() {
       FlowerPower.stopDiscoverAll(onDiscover);
       console.log("End of discovery")
       callback();
     },waitTimeoutMs));
-
-    for(i = 0; i < uuidTab.length; i++){
-      if(uuidTab[i] === flowerPower.uuid) {
-        tab.splice(i,0,flowerPower);
-        console.log("SPLICE i=" + i + ' fp=' + flowerPower.uuid)
-      }
-    }
-    loop1 ++;
   });
 },
 
+// ---------------------------------------------------------------------------------
+// try to connect and readout each sensor and push data to cloud
+//
 function(callback) {
-  function analyser(i) {
-    if(i < tab.length) {
-      if(tab[i] == 1) {
-        loop2 ++;
-        analyser(loop2);
-      }
-      else{
-        async.series([
-          function(callback) {
-            tab[i].on('disconnect', function() {
-              console.log('disconnected!');
-              callback();
-            });
-            console.log('connectAndSetup' + tab[i]);
-            tab[i].connectAndSetup(callback);
-            for(x = 0; x < tabIndex.length; x++){
-              if(uuidTab[x] === tab[i].uuid) {
-              order = x;
-              console.log("order id x=" + order)
-              }
-            }
-          },
+  async.eachSeries(uuidTab, function iterator(uuid, callback) {
+    connectAndReadout(uuid,callback);
+  }, function done() {
+    
+    callback();
+  });
+}
 
-/*          function(callback) {
-            console.log('readFriendlyName');
-            tab[i].readFriendlyName(function(friendlyName) {
-              tab[i].writeFriendlyName(friendlyName, callback);
-            });
-          },
+//////////////////////////////////////////////////////////////////////////////////// 
+], function (error,result) {
+  if (error) { 
+    console.log("Error in main waterfall: ",error);
+  } else {
+    console.log("Main waterfall dried out. Goodbye.");
+  }
+  process.exit(0);
+}); //async.waterfall
 
-          function(callback) {
-            tab[i].getHistoryNbEntries(function(data) {
-              NbrEntries = data;
-            console.log('getHistoryNbEntries: ' + NbrEntries);
-              callback();
+
+
+
+/* *************************************************************************************************
+ *
+ * connect and readout one sensor and upload data to cloud 
+ *
+ * uuid:      index key to sensors[]
+ * callback:  called at end of execution
+ *
+ * ************************************************************************************************/
+
+function connectAndReadout(uuid, callback)
+{
+  if (devices[uuid].state != 'confirmed') {
+    console.log("** Internal error: Expected device uuid="+uuid+" to be confirmed, was \""+devices[uuid].state+"\" instead.");
+    callback();
+    return;
+  } 
+
+  var uuid=uuid;
+  var sensor= devices[uuid].sensor;
+  var lastEntry= 0;
+  var startIdx= 0;
+  var currentID= '';
+  var sessionStartIndex= '';
+  var sessionPeriod = "";
+  var sUpTime = "";
+  var historic = null;
+  var NbrEntries = 0;
+
+
+
+  console.log("");
+  console.log("Now reading out uuid="+uuid+", name=\""+sensor.name+"\": ");
+
+  devices[uuid].state= 'connect';
+
+  async.waterfall([
+    //--------------------------------------------------------------
+    function(callback) { // -------- waterfall task: connectAndSetup
+            
+            sensor.on('disconnect', function() {
+              if (1<=debug) console.log('peripheral disconnected!');
+              callback('disconnect'); // check in final callback, if this is an error
             });
-          },
-*/
-          function(callback) {
-            tab[i].getHistoryLastEntryIdx(function(data) {
+            if (1<=debug) console.log('connectAndSetup ' + uuid);
+
+            sensor.connectAndSetup(callback);
+    },
+
+    //---------------------------------------------------------------------
+    function(callback) { // -------- waterfall task: getHistoryLastEntryIdx
+      devices[uuid].state= 'connected';
+      try {
+            sensor.getHistoryLastEntryIdx(function(data) {
               lastEntry = data;
               console.log('getHistoryLastEntryIdx:' + lastEntry);
-              startIdx = tabIndex[order];
+              startIdx = devices[uuid].api.historyIndex;
               callback();
             });
-          },
+      } catch(error) { callback("getHistoryLastEntryIdx: "+error.message); }
+    },
 
-          function(callback) {
-            tab[i].getHistoryCurrentSessionID(function(data) {
+    //-------------------------------------------------------------------------
+    function(callback) { // -------- waterfall task: getHistoryCurrentSessionID
+      try {
+            sensor.getHistoryCurrentSessionID(function(data) {
               currentID = data;
               console.log('getHistoryCurrentSessionID: ' + currentID);
               callback();
             });
-          },
+      } catch(error) { callback("getHistoryCurrentSessionID: "+error.message); }
+    },
 
-          function(callback) {
-            tab[i].getHistoryCurrentSessionStartIdx(function(data) {
+    //-------------------------------------------------------------------------------
+    function(callback) { // -------- waterfall task: getHistoryCurrentSessionStartIdx
+      try {
+            sensor.getHistoryCurrentSessionStartIdx(function(data) {
               sessionStartIndex = data;
               console.log('getHistoryCurrentSessionStartIdx: ' + sessionStartIndex);
               callback();
             });
-          },
+      } catch(error) { callback("getHistoryCurrentSessionStartIdx: "+error.message); }
+    },
 
-          function(callback) {
-            tab[i].getHistoryCurrentSessionPeriod(function(data) {
+    //-----------------------------------------------------------------------------
+    function(callback) { // -------- waterfall task: getHistoryCurrentSessionPeriod
+      try {
+            sensor.getHistoryCurrentSessionPeriod(function(data) {
               sessionPeriod = data;
               console.log('getHistoryCurrentSessionPeriod: ' + sessionPeriod);
               callback();
             });
-          },
+      } catch(error) { callback("getHistoryCurrentSessionPeriod: "+error.message); }
+    },
 
-          function(callback) {
-            tab[i].getStartupTime(function(startupTime) {
+    //--------------------------------------------------------------
+    function(callback) { // -------- waterfall task: getStartupTime
+      try {
+            sensor.getStartupTime(function(startupTime) {
               sUpTime = startupTime;
               console.log('getStartUpTime: ' + sUpTime);
               callback();
             });
-          },
+      } catch(error) { callback("getStartupTime: "+error.message); }
+    },
 
-          function(callback) {
+    //--------------------------------------------------------------
+    function(callback) { // -------- waterfall task: getHistory
+      try {
             console.log('getHistory original startIdx=' + startIdx);
             if (startIdx == null || (lastEntry - startIdx) > maxIndexRecovery || (startIdx - lastEntry) > 0) {
               startIdx = lastEntry - defaultIndexRecovery;
@@ -234,43 +263,90 @@ function(callback) {
             }
             if ((startIdx - lastEntry) == 0) {
               console.log("All samples already uploaded");
-              historic = '';
-/*              console.log('disconnect');
-              tab[i].disconnect(callback);
-              loop2 ++;
-              analyser(loop2);
-*/            }
+              historic = null;
+              callback();
+              //callback("no data to upload");
+            }
             else {
-              tab[i].getHistory(startIdx, function(error, history) {
-                historic = history;
-                callback();
+              sensor.getHistory(startIdx, function(error, history) {
+                devices[uuid].state= 'download';
+                // uncomment for debugging, this REALLY produces a lot of noise..
+                //console.log("getHistory uuid="+uuid+": received ",history);
+                
+                // apparently, getHistory() may issue multiple calls to callback() 
+                // - so, we accept only the first:
+                if (historic==null) {
+                  if (1<=debug) console.log("got all data from "+uuid);
+                  devices[uuid].state= 'disconnect'; // disconnects are OK from now on
+                  historic = history;
+                  callback();
+                }
               });
             }
-          },
+      } catch(error) { callback("getHistory: "+error.message); }
+    },
 
-/*          function (callback) {
-            fs.writeFile(tab[i].uuid+'.txt', historic, function (err) {
-              if (err) throw err;
-              callback();
-            });
-          },
-*/
-          function (callback) {
-            api.uploadGarden(tabSensors[order], user_config_version1, sUpTime, historic, currentID, sessionPeriod, sessionStartIndex, function(err) {
-              console.log('uploaded to sensor '+ tabSensors[order]);
-              console.log('disconnect');
-              tab[i].disconnect(callback);
-              loop2 ++;
-              analyser(loop2);
-            });
-          },
-        ]);
+    //--------------------------------------------------------------
+    function(callback) { // -------- waterfall task: callbackScavange
+      //
+      // This is really only a workaround to avoid multiple executions
+      // of uploadGarden. 
+      // Apparently, getHistory() may call callback() multiple times.
+      // We already try to handle this by testing historic, but still...
+      //
+      if (devices[uuid].state== 'downloaded') { 
+        console.log("** ERROR: callbackScavange triggered for "+uuid);
+        return;
       }
+      devices[uuid].state= 'downloaded';
+      callback();
+    },
+
+    //--------------------------------------------------------------
+    function(callback) { // -------- waterfall task: uploadGarden
+      try {
+          var sensorSerial= devices[uuid].api.sensorSerial;
+          if (!dry_run) {
+            if (historic) {
+              console.log('now uploading to sensor '+sensorSerial);
+              api.uploadGarden(sensorSerial, user_config_version1, sUpTime, historic, currentID, sessionPeriod, sessionStartIndex, function(err) {
+                console.log('uploaded to sensor '+ sensorSerial);
+                callback();
+              });
+            } else {
+              console.log('skipping upload to sensor '+sensorSerial);
+              callback(); //nothing to upload, proceed to disconnect
+            }
+          } 
+          else {
+            console.log("dry-run, no upload for "+sensorSerial+" w/ user_config_version="+user_config_version1+".");
+            callback();
+          }
+      } catch(error) { callback(": "+error.message); }
+    },
+
+    //--------------------------------------------------------------
+    function(callback) { // -------- waterfall task: disconnect
+      if (1<=debug) console.log("final steps for "+uuid);
+      devices[uuid].state= 'disconnect';
+      try {
+            console.log("disconnect from "+uuid);
+            sensor.disconnect(callback);
+      } catch(error) { callback(": "+error.message); return; }
     }
-    else {
-      process.exit(0);
-    }
-  }
-  analyser(loop2);
+
+  ],
+  ////////////////////////////////////////////////////////////////// 
+  function(error, result) {
+    if (error) {
+      if ((error=='disconnect') && (devices[uuid].state == 'disconnect')) {
+        if (2<=debug) console.log("(catching expected disconnect of "+uuid+")");
+        return; // no further callbacks!
+      }
+      console.log("** Early end of waterfall (uuid="+uuid+"): ",error);
+    } else {
+      console.log("Finishing waterfall (uuid="+uuid+") as planned.");
+    } 
+    callback();
+  });
 }
-]);
